77fe629503a1b6fa5039b9de406eac536312fde1
kanmear <kanmear+github@gmail.com>
2022-11-23 23:36:35 +0300

    learning basics

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
+// #include "fireworks.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <iostream>
 
 using namespace sf;
 
+void drawFireworks(RenderWindow &window, CircleShape &shape);
+
 int main() {
   RenderWindow window(VideoMode(200, 200), "fireworks");
   window.setFramerateLimit(120);
+
+  CircleShape shape(100.f);
+  shape.setFillColor(Color::Red);
 
   while (window.isOpen()) {
     Event event;
@@ -14,11 +22,19 @@ int main() {
         window.close();
     }
 
-    std::cout << "debug log test";
+    // std::cout << "debug log test";
     window.clear();
+
+    drawFireworks(window, shape);
 
     window.display();
   }
 
   return 0;
+}
+
+void drawFireworks(RenderWindow &window, CircleShape &shape) {
+  shape.move(0.3f, 0.f);
+
+  window.draw(shape);
 }
