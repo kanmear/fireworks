f58035b67f7a0c0c62958ffcce23bf1633ce0dba
kanmear <kanmear+github@gmail.com>
2022-11-24 23:48:35 +0300

    fix: typo

--- fireworks.cpp
+++ fireworks.cpp
@@ -5,8 +5,18 @@
 
 using namespace sf;
 
+void launchProjectile(CircleShape &shape);
+
 void drawFireworks(RenderWindow &window, CircleShape &shape) {
-  shape.move(0.3f, 0.f);
+  launchProjectile(shape);
 
   window.draw(shape);
+}
+
+void launchProjectile(CircleShape &shape) {
+  shape.move(0.3f, 0.f);
+}
+
+void move(CircleShape &shape, float x, float y) {
+  shape.move(x, y);
 }
