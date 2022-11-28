35814a97aaae58e0de12f8918939f108bffac710
kanmear <kanmear+github@gmail.com>
2022-11-28 23:24:28 +0300

    feat: extremely simple (or rather wrong) physics

--- main.cpp
+++ main.cpp
@@ -9,34 +9,48 @@ using namespace sf;
 int main() {
   RenderWindow window(VideoMode(200, 200), "fireworks");
   window.setFramerateLimit(120);
+  window.setKeyRepeatEnabled(false);
+  
+  float acceleration = -0.f;
+  float gravity = 9.8f;
 
-  CircleShape shape(100.f);
+  CircleShape shape(50.f);
   shape.setFillColor(Color::Red);
 
+  std::cout << std::to_string(window.getSize().y) + "\n";
   while (window.isOpen()) {
-    window.setKeyRepeatEnabled(false);
-
     Event event;
     while (window.pollEvent(event)) {
       if (event.type == Event::Closed)
         window.close();
       
-      // if (event.type == Event::KeyPressed) {
-      //   if (event.key.code == Keyboard::F) {
-      //     std::cout << "F pressed\n";
-      //     move(shape, 20.f, 20.f);
-      //   }
-      // }
+      // single input per press
+      if (event.type == Event::KeyPressed) {
+        if (event.key.code == Keyboard::F) {
+          std::cout << "F pressed\n";
+          acceleration -= 15.f;
+          shape.move(0.f, -100.f);
+        }
+      }
     }
-
+    
+    if (acceleration < 0.f) {
+      acceleration += 0.3f;
+    }
+    
+    std::cout << std::to_string(shape.getPosition().y) + "\n";
+    if (shape.getPosition().y + 100.f < window.getSize().y) {
+      shape.move(0.f, acceleration + gravity);
+    }
     // std::cout << "debug log test\n";
     window.clear();
 
-    if (Keyboard::isKeyPressed(Keyboard::Key::F)) {
-          std::cout << "F pressed\n";
-          move(shape, 20.f, 20.f);
-    }
-    drawFireworks(window, shape);
+    // if (Keyboard::isKeyPressed(Keyboard::Key::F)) {
+    //       std::cout << "F pressed\n";
+    //       accelerate(shape);
+    // }
+    // drawFireworks(window, shape);
+    window.draw(shape);
 
     window.display();
   }
