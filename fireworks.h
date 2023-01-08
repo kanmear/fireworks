8b7047f89beb1065fb4003bcfa1b04995d450def
kanmear <kanmear+github@gmail.com>
2023-01-08 17:41:59 +0300

    fix: remove redundant files

--- fireworks.h
+++
@@ -1,8 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-
-using namespace sf;
-
-void drawFireworks(RenderWindow &window, CircleShape &shape);
-void move(CircleShape &shape, float x, float y);
